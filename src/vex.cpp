4db67e04ff9301e342c1ed2711bf0261351e1259
Alex Ye <albertexye@gmail.com>
2024-09-12 14:09:35 -0400

    feat: added motors

--- src/vex.cpp
+++ src/vex.cpp
@@ -14,4 +14,10 @@
 namespace Global {
     vex::competition competition{};
     vex::brain brain{};
+    vex::motor left_motor1{ 0, vex::gearSetting::ratio18_1, false };
+    vex::motor left_motor2{ 0, vex::gearSetting::ratio18_1, false };
+    vex::motor left_motor3{ 0, vex::gearSetting::ratio18_1, false };
+    vex::motor right_motor1{ 0, vex::gearSetting::ratio18_1, false };
+    vex::motor right_motor2{ 0, vex::gearSetting::ratio18_1, false };
+    vex::motor right_motor3{ 0, vex::gearSetting::ratio18_1, false };
 }
