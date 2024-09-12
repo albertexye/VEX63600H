9177fff0fbe8703796ae51edc5917b084f69ee00
Alex Ye <albertexye@gmail.com>
2024-09-11 22:23:12 -0400

    refactor: make the default template match the project code guidelines
    The details specified in CONTRIBUTING.md are applied to the code now.

---
+++ include/config.h
@@ -0,0 +1,12 @@
+/**
+ * @file config.h
+ * @author Alex Ye (albertexye@gmail.com)
+ * @brief Project configuration file
+ * @version 0.1
+ * @date 2024-09-11
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#pragma once
---
+++ src/vex.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file vex.cpp
+ * @author Alex Ye (albertexye@gmail.com)
+ * @brief Instantiating the vex devices
+ * @version 0.1
+ * @date 2024-09-11
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "vex.h"
+
+namespace Global {
+    vex::competition competition{};
+    vex::brain brain{};
+}
